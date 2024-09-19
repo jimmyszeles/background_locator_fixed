e7c3512a476bf26c53e662faffb28a97ff7ad621
Jimmy Szeles <jimmy_szeles@hotmail.com>
2024-09-19 23:01:05 +0200

    Upgraded sdk versions

--- android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
@@ -8,7 +8,7 @@ import java.util.HashMap
 
 class LocationParserUtil {
     companion object {
-        fun getLocationMapFromLocation(location: Location): HashMap<Any, Any> {
+        fun getLocationMapFromLocation(location: Location): HashMap<String, Any>? {
             var speedAccuracy = 0f
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 speedAccuracy = location.speedAccuracyMetersPerSecond
@@ -19,20 +19,20 @@ class LocationParserUtil {
             }
 
             return hashMapOf(
-                    Keys.ARG_IS_MOCKED to isMocked,
-                    Keys.ARG_LATITUDE to location.latitude,
-                    Keys.ARG_LONGITUDE to location.longitude,
-                    Keys.ARG_ACCURACY to location.accuracy,
-                    Keys.ARG_ALTITUDE to location.altitude,
-                    Keys.ARG_SPEED to location.speed,
-                    Keys.ARG_SPEED_ACCURACY to speedAccuracy,
-                    Keys.ARG_HEADING to location.bearing,
-                    Keys.ARG_TIME to location.time.toDouble(),
-                    Keys.ARG_PROVIDER to location.provider,
+                    Keys.ARG_IS_MOCKED to isMocked as Any,
+                    Keys.ARG_LATITUDE to location.latitude as Any,
+                    Keys.ARG_LONGITUDE to location.longitude as Any,
+                    Keys.ARG_ACCURACY to location.accuracy as Any,
+                    Keys.ARG_ALTITUDE to location.altitude as Any,
+                    Keys.ARG_SPEED to location.speed as Any,
+                    Keys.ARG_SPEED_ACCURACY to speedAccuracy as Any,
+                    Keys.ARG_HEADING to location.bearing as Any,
+                    Keys.ARG_TIME to location.time.toDouble() as Any,
+                    Keys.ARG_PROVIDER to location.provider as Any,
             )
         }
 
-        fun getLocationMapFromLocation(location: LocationResult?): HashMap<Any, Any>? {
+        fun getLocationMapFromLocation(location: LocationResult?): HashMap<String, Any>? {
             val firstLocation = location?.lastLocation ?: return null
 
             var speedAccuracy = 0f
@@ -45,15 +45,16 @@ class LocationParserUtil {
             }
 
             return hashMapOf(
-                    Keys.ARG_IS_MOCKED to isMocked,
-                    Keys.ARG_LATITUDE to firstLocation.latitude,
-                    Keys.ARG_LONGITUDE to firstLocation.longitude,
-                    Keys.ARG_ACCURACY to firstLocation.accuracy,
-                    Keys.ARG_ALTITUDE to firstLocation.altitude,
-                    Keys.ARG_SPEED to firstLocation.speed,
-                    Keys.ARG_SPEED_ACCURACY to speedAccuracy,
-                    Keys.ARG_HEADING to firstLocation.bearing,
-                    Keys.ARG_TIME to firstLocation.time.toDouble())
+                    Keys.ARG_IS_MOCKED to isMocked as Any,
+                    Keys.ARG_LATITUDE to firstLocation.latitude as Any,
+                    Keys.ARG_LONGITUDE to firstLocation.longitude as Any,
+                    Keys.ARG_ACCURACY to firstLocation.accuracy as Any,
+                    Keys.ARG_ALTITUDE to firstLocation.altitude as Any,
+                    Keys.ARG_SPEED to firstLocation.speed as Any,
+                    Keys.ARG_SPEED_ACCURACY to speedAccuracy as Any,
+                    Keys.ARG_HEADING to firstLocation.bearing as Any,
+                    Keys.ARG_TIME to firstLocation.time.toDouble() as Any,
+                )
         }
     }
 }
